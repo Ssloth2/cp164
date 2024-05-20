a2220e72dcd6264f7283f09e5420655f0da411cc
Ssloth2 <134955714+Ssloth2@users.noreply.github.com>
2024-05-20 19:42:03 -0400

    Add files via upload

---
+++ shah1402_trial/src/trial.py
@@ -0,0 +1,11 @@
+"""
+-------------------------------------------------------
+Midterm Trial
+-------------------------------------------------------
+Author: Saad Shah
+ID:     169071402
+Email:  shah1402@mylaurier.ca
+__updated__ = "2023-06-12"
+-------------------------------------------------------
+"""
+print("Congratulations, you imported this project correctly.")
